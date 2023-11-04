934511b8ea10bcb5f2042a1c0ba9b38829e1e4c9
Tim Chandler <tim@code-distortion.net>
2023-11-04 11:51:28 +1100

    Initial commit

---
+++ tests/PHPUnitTestCase.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CodeDistortion\TestRepo\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * The PHPUnit test case.
+ */
+abstract class PHPUnitTestCase extends TestCase
+{
+}
---
+++ tests/Unit/PathUnitTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodeDistortion\TestRepo\Tests\Unit;
+
+use CodeDistortion\TestRepo\Tests\PHPUnitTestCase;
+
+/**
+ * @phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+ */
+class PathUnitTest extends PHPUnitTestCase
+{
+    /**
+     * Test something.
+     *
+     * @test
+     *
+     * @return void
+     */
+    public function test_something(): void {
+        self::assertTrue(true);
+    }
+}
